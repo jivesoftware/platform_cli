89c1c29096551f0490c3fb81b47d4f4cdaa3aca4
David Tweet <david.tweet@jivesoftware.com>
2014-09-09 13:50:33 -0700

    Bump version to 1.0.11.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'platform_cli',
-    version = '1.0.10',
+    version = '1.0.11',
     packages = ['platform_cli'],
     install_requires = [
         'psutil >= 0.6.1',
